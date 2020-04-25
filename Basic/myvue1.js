1e53f00ff72cb641e4264c59704be06971273551
Ashraful <ashrafulislam.mbstu123@gmail.com>
2020-04-25 22:47:27 +0600

    Instance to computed

---
+++ Basic/myvue1.js
@@ -0,0 +1,37 @@
+var app = new Vue({
+  el: '#container',
+  data: {
+    brand:'Apple',
+    product: 'iPhone 11',
+    selectedVarient:0,
+    cart:0,
+    detail:["5.7\" Display","3GB Ram","Quad-core Processor" ],
+    varients:[
+      {color_id:10,color:'red',color_img:'Basic/red.png',qty:5},
+      {color_id:11,color:'green',color_img:'Basic/green.png',qty:0},
+      {color_id:12,color:'black',color_img:'Basic/black.png',qty:10}
+    ]
+
+  },
+
+  methods:{
+    addtoCart:function(){
+      this.cart += 1
+    },
+    colorVarient:function(index){
+      this.selectedVarient=index
+    }
+  },
+  computed:{
+    title(){
+      return this.brand +" "+this.product
+    },
+    image(){
+      return this.varients[this.selectedVarient].color_img
+    },
+    stock(){
+      return this.varients[this.selectedVarient].qty
+    }
+  }
+
+})
