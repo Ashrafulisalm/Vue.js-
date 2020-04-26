92cfdc4e4fa6cca00e4a87fe49dd2cc36469f482
Ashraful <ashrafulislam.mbstu123@gmail.com>
2020-04-26 23:39:37 +0600

    Vue form v-model

---
+++ Form/form.js
@@ -0,0 +1,32 @@
+Vue.component('user-review',{
+  template:'',
+  data(){
+    return {
+      name:null,
+      rating:null,
+      review:null
+    }
+  }
+})
+
+var app =new Vue({
+  el:'.form',
+  data:{
+    name:null,
+    rating:null,
+    review:null,
+  },
+  methods:{
+    onSubmit(){
+      let productReview = {
+        name:this.name,
+        rating:this.rating,
+        review:this.review
+      }
+      this.name=null,
+      this.rating=null,
+      this.review=null
+    }
+
+  }
+})
